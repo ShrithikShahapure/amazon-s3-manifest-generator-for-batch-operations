7eb772e7b52834e763913b5a0faff3a52128ec2f
ShrithikShahapure <svshrithik12@gmail.com>
2025-02-19 21:57:00 -0500

    Updated SDK from V2 to V3; added a .gitignore, added a condition to check and ensure filetype is a csv file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
+const { S3 } = require('@aws-sdk/client-s3');
+const { createWriteStream } = require('fs');
 
-var s3 = new AWS.S3();
+var s3 = new S3();
 
 const SRC_BUCKET = process.argv[2];
 const PREFIX = process.argv[3];
@@ -11,25 +11,25 @@ let stream = null;
 
 async function list(token) {
     let params = {
-          Bucket: SRC_BUCKET
-        };    
+        Bucket: SRC_BUCKET
+    };
 
-    if(PREFIX != "/") {
+    if (PREFIX != "/") {
         params.Prefix = PREFIX;
     }
 
-    if(!!token) {
+    if (!!token) {
         params.ContinuationToken = token;
     }
-    
-    return s3.listObjectsV2(params).promise();   
+
+    return s3.listObjectsV2(params);
 }
 
 function write(objects) {
     objects.Contents.forEach(object => {
         let line = format(object);
         stream.write(line);
-    }); 
+    });
 }
 
 function format(object) {
@@ -38,26 +38,31 @@ function format(object) {
 
 
 function help() {
-    if(process.argv.length != 5) {
+    if (process.argv.length != 5) {
         console.log("Usage: node index.js source_bucket bucket_prefix output_file");
+        process.exit(1);
+    }
+
+    if (!process.argv[5] || !process.argv[5].includes('.csv')) {
+        console.log("Error: Fifth argument must be a csv");
         process.exit(1);
     }
 }
 
 async function main() {
     help();
-    
+
     let hasMoreFiles = false;
     let continuationToken = null;
-    stream = fs.createWriteStream(OUTPUT_FILE, { flags: 'a' });
-    
+    stream = createWriteStream(OUTPUT_FILE, { flags: 'a' });
+
     do {
         let objects = await list(continuationToken);
         write(objects);
         hasMoreFiles = objects.IsTruncated;
-        continuationToken = (hasMoreFiles) ? objects.NextContinuationToken:null;
+        continuationToken = (hasMoreFiles) ? objects.NextContinuationToken : null;
         process.stdout.write('.');
-    } while(hasMoreFiles);
+    } while (hasMoreFiles);
 
     stream.end();
     process.stdout.write('\n');
